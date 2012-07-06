dfaa99eb2672918c6183ee42587de776b3d38706
Alex Bilbie <alex@alexbilbie.com>
2012-07-06 13:31:26 +0100

    Converted tabs to spaces

--- src/oauth2server/Client.php
+++ src/oauth2server/Client.php
@@ -4,22 +4,22 @@ namespace LNCD\OAuth2server;
 
 class Client
 {
-	protected $id;
-	protected $secret;
-	protected $redirect_uri;
+    protected $id;
+    protected $secret;
+    protected $redirect_uri;
 
-	public function __construct()
-	{
+    public function __construct()
+    {
 
-	}
+    }
 
-	public function validate(array $details)
-	{
+    public function validate(array $details)
+    {
 
-	}
+    }
 
-	public function redirectUri(string $redirectUri, array $params, $queryDelimeter = '?')
-	{
-		// Generates the redirect uri with appended params
-	}
+    public function redirectUri(string $redirectUri, array $params, $queryDelimeter = '?')
+    {
+        // Generates the redirect uri with appended params
+    }
 }
