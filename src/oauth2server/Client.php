48a068b999c24dd9e26bbd02dcc2ccf290e314d1
Alex Bilbie <alex@alexbilbie.com>
2012-07-06 13:40:59 +0100

    $queryDelimeter should be a string

--- src/oauth2server/Client.php
+++ src/oauth2server/Client.php
@@ -18,7 +18,7 @@ class Client
 
     }
 
-    public function redirectUri(string $redirectUri, array $params, $queryDelimeter = '?')
+    public function redirectUri(string $redirectUri, array $params, string $queryDelimeter = '?')
     {
         // Generates the redirect uri with appended params
     }
