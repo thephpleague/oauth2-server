4eb0f8b596b031c9a78873fb3fc34bec99f77742
Alex Bilbie <alex@alexbilbie.com>
2012-07-06 16:44:07 +0100

    Added exceptions classes

--- src/oauth2server/Server.php
+++ src/oauth2server/Server.php
@@ -2,6 +2,12 @@
 
 namespace LNCD\OAuth2server;
 
+class OAuthSererClientException extends Exception {}
+
+class OAuthServerUserException extends Exception {}
+
+class OAuthServerException extends Exception {}
+
 class Server
 {
 
