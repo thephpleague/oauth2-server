3eb408bf6f034fb7847107e2c7ed3db324e9d12d
Alex Bilbie <alex@alexbilbie.com>
2012-07-06 13:38:05 +0100

    Split functions so line lengths aren't so long

--- src/oauth2server/DbInterface.php
+++ src/oauth2server/DbInterface.php
@@ -2,29 +2,67 @@
 
 interface OAuth2ServerDatabase
 {
-    public function validateClient(string $clientId, $clientSecret, $redirectUri);
+    public function validateClient(
+        string $clientId,
+        $clientSecret,
+        $redirectUri
+    );
 
-    public function newSession(string $clientId, string $redirectUri, string $type = 'user', string $typeId, $authCode, $accessToken, string $stage = 'request');
+    public function newSession(
+        string $clientId,
+        string $redirectUri,
+        string $type = 'user',
+        string $typeId,
+        $authCode,
+        $accessToken,
+        string $stage = 'request'
+    );
 
-    public function updateSession(string $clientId, string $type = 'user', string $typeId, $authCode, $accessToken, string $stage);
+    public function updateSession(
+        string $clientId,
+        string $type = 'user',
+        string $typeId,
+        $authCode,
+        $accessToken,
+        string $stage
+    );
 
-    public function deleteSession(string $clientId, string $typeId);
+    public function deleteSession(
+        string $clientId,
+        string $typeId
+    );
 
-    public function validateAuthCode(string $clientId, string $redirectUri, string $authCode);
+    public function validateAuthCode(
+        string $clientId,
+        string $redirectUri,
+        string $authCode
+    );
 
     public function getAccessToken(int $sessionId);
 
     public function removeAuthCode(int $sessionId);
 
-    public function setAccessToken(int $sessionId, string $accessToken);
+    public function setAccessToken(
+        int $sessionId,
+        string $accessToken
+    );
 
-    public function addSessionScope(int $sessionId, string $scope);
+    public function addSessionScope(
+        int $sessionId,
+        string $scope
+    );
 
     public function getScope(string $scope);
 
-    public function updateSessionScopeAccessToken(int $sesstionId, string $accessToken);
+    public function updateSessionScopeAccessToken(
+        int $sesstionId,
+        string $accessToken
+    );
 
     public function accessTokenScopes(string $accessToken);
 
-    public function validateUser(string $username, string $password);
+    public function validateUser(
+        string $username,
+        string $password
+    );
 }
