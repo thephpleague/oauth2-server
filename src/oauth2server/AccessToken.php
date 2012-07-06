6d7aa9726d60990f22a6b6cd97ea0471e83e9000
Alex Bilbie <alex@alexbilbie.com>
2012-07-06 13:30:10 +0100

    Coverted tabs to spaces

--- src/oauth2server/AccessToken.php
+++ src/oauth2server/AccessToken.php
@@ -4,23 +4,23 @@ namespace LNCD\OAuth2server;
 
 class AccessToken
 {
-	function __construct()
-	{
+    function __construct()
+    {
 
-	}
+    }
 
-	public function get(int $sessionId)
-	{
-		// returns an access token that the user may already have (else generate a new one)
-	}
+    public function get(int $sessionId)
+    {
+        // returns an access token that the user may already have (else generate a new one)
+    }
 
-	public function validate(string $accessToken, array $scopes)
-	{
-		// tests if an access token is valid
-	}
+    public function validate(string $accessToken, array $scopes)
+    {
+        // tests if an access token is valid
+    }
 
-	private function set(int $sessionId)
-	{
-		// generate a new access token
-	}
+    private function set(int $sessionId)
+    {
+        // generate a new access token
+    }
 }
