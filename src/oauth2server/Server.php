3c6ad24f22511d6ce73a3e82a7a71d1d0cf0954e
Alex Bilbie <alex@alexbilbie.com>
2012-07-12 14:57:29 +0100

    Changed the namespace to oauth2server

--- src/oauth2server/Server.php
+++ src/oauth2server/Server.php
@@ -20,7 +20,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-namespace OAuth2;
+namespace oauth2server;
 
 class OAuthServerClientException extends \Exception {}
 
