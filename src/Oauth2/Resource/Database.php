519d20f0a51b09fd009d579fb54b1696bee19ae8
Alex Bilbie <alex@alexbilbie.com>
2012-08-14 16:34:43 +0100

    Changed indent to spaces

--- src/Oauth2/Resource/Database.php
+++ src/Oauth2/Resource/Database.php
@@ -4,16 +4,16 @@ namespace Oauth2\Resource;
 
 interface Database
 {
-	/**
-	 * Validate an access token and return the session details.
-	 * 
-	 * Database query:
-	 * 
-	 * <code>
-	 * SELECT id, owner_type, owner_id FROM oauth_sessions WHERE access_token =
-	 *  $accessToken AND stage = 'granted' AND
-	 *  access_token_expires > UNIX_TIMESTAMP(now())
-	 * </code>
+    /**
+     * Validate an access token and return the session details.
+     * 
+     * Database query:
+     * 
+     * <code>
+     * SELECT id, owner_type, owner_id FROM oauth_sessions WHERE access_token =
+     *  $accessToken AND stage = 'granted' AND
+     *  access_token_expires > UNIX_TIMESTAMP(now())
+     * </code>
      * 
      * Response:
      * 
@@ -25,35 +25,35 @@ interface Database
      *     [owner_id] => (string) The session owner's ID
      * )
      * </code>
-	 * 
-	 * @param  string     $accessToken The access token
-	 * @return array|bool              Return an array on success or false on failure
-	 */
-	public function validateAccessToken($accessToken);
+     * 
+     * @param  string     $accessToken The access token
+     * @return array|bool              Return an array on success or false on failure
+     */
+    public function validateAccessToken($accessToken);
 
-	/**
-	 * Returns the scopes that the session is authorised with.
-	 * 
-	 * Database query:
-	 * 
-	 * <code>
-	 * SELECT scope FROM oauth_session_scopes WHERE access_token =
-	 *  '291dca1c74900f5f252de351e0105aa3fc91b90b'
-	 * </code>
-	 * 
-	 * Response:
-	 * 
-	 * <code>
-	 * Array
-	 * (
-	 * 		[0] => (string) A scope
-	 *   	[1]	=> (string) Another scope
-	 *    	...
-	 * )
-	 * </code>
-	 * 
-	 * @param  int   $sessionId The session ID
-	 * @return array            A list of scopes
-	 */
-	public function sessionScopes($sessionId);
+    /**
+     * Returns the scopes that the session is authorised with.
+     * 
+     * Database query:
+     * 
+     * <code>
+     * SELECT scope FROM oauth_session_scopes WHERE access_token =
+     *  '291dca1c74900f5f252de351e0105aa3fc91b90b'
+     * </code>
+     * 
+     * Response:
+     * 
+     * <code>
+     * Array
+     * (
+     *      [0] => (string) A scope
+     *      [1] => (string) Another scope
+     *      ...
+     * )
+     * </code>
+     * 
+     * @param  int   $sessionId The session ID
+     * @return array            A list of scopes
+     */
+    public function sessionScopes($sessionId);
 }
