ec191cffe11f5b7b9605eba46352ef4194fd1f69
Alex Bilbie <alex@alexbilbie.com>
2012-10-05 15:02:56 +0100

    Cleanup
    Signed-off-by: Alex Bilbie <alex@alexbilbie.com>

--- src/Oauth2/Client/Token.php
+++ src/Oauth2/Client/Token.php
@@ -2,22 +2,11 @@
 
 namespace Oauth2\Client;
 
-/**
- * OAuth2 Token
- *
- * @package    OAuth2
- * @category   Token
- * @author     Phil Sturgeon
- * @copyright  (c) 2011 HappyNinjas Ltd
- */
-
 abstract class Token
 {
 
 	/**
 	 * Create a new token object.
-	 *
-	 *     $token = OAuth2_Token::factory($name);
 	 *
 	 * @param   string  token type
 	 * @param   array   token options
@@ -25,9 +14,7 @@ abstract class Token
 	 */
 	public static function factory($name = 'access', array $options = null)
 	{
-		$name = ucfirst(strtolower($name));
-
-		include_once 'Token/'.$name.'.php';
+		include_once 'Token/'.ucfirst(strtolower($name)).'.php';
 
 		$class = $name.'Token';
 
@@ -36,9 +23,6 @@ abstract class Token
 
 	/**
 	 * Return the value of any protected class variable.
-	 *
-	 *     // Get the token secret
-	 *     $secret = $token->secret;
 	 *
 	 * @param   string  variable name
 	 * @return  mixed
@@ -50,9 +34,6 @@ abstract class Token
 
 	/**
 	 * Return a boolean if the property is set
-	 *
-	 *     // Get the token secret
-	 *     if ($token->secret) exit('YAY SECRET');
 	 *
 	 * @param   string  variable name
 	 * @return  bool
