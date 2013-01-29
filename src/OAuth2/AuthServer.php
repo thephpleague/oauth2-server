846a008c76cf8d080d9cba7a2d823ee7d7457cd5
Alex Bilbie <hello@alexbilbie.com>
2013-01-29 14:18:13 +0000

    Explanation of the scope delimiter parameter

--- src/OAuth2/AuthServer.php
+++ src/OAuth2/AuthServer.php
@@ -8,6 +8,14 @@ use OAuth2\Storage\ScopeInterface;
 
 class AuthServer
 {
+    /**
+     * The delimeter between scopes specified in the scope query string parameter
+     *
+     * The OAuth 2 specification states it should be a space but that is stupid
+     * and everyone excepted Google use a comma instead.
+     *
+     * @var string
+     */
     protected $scopeDelimeter = ',';
 
     protected $expiresIn = 3600;
