5e91b95cb35ed930568c43563135cdc4990dbc46
Alex Bilbie <hello@alexbilbie.com>
2013-01-29 14:25:49 +0000

    Updated ScopeInterface with get method

--- src/OAuth2/Storage/ScopeInterface.php
+++ src/OAuth2/Storage/ScopeInterface.php
@@ -4,5 +4,29 @@ namespace OAuth2\Storage;
 
 interface ScopeInterface
 {
-    
+    /**
+     * Return information about a scope
+     *
+     * Example SQL query:
+     *
+     * <code>
+     * SELECT * FROM scopes WHERE scope = $scope
+     * </code>
+     *
+     * Response:
+     *
+     * <code>
+     * Array
+     * (
+     *     [id] => (int) The scope's ID
+     *     [scope] => (string) The scope itself
+     *     [name] => (string) The scope's name
+     *     [description] => (string) The scope's description
+     * )
+     * </code>
+     *
+     * @param  string     $scope The scope
+     * @return bool|array If the scope doesn't exist return false
+     */
+    public function get($scope);
 }
