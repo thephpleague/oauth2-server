2c1dedfe8a9f9b8d8850db1b4c1d5557d326fd32
Alex Bilbie <hello@alexbilbie.com>
2013-02-01 14:56:36 +0000

    Updated grantTypeInterface

--- src/OAuth2/Grant/GrantTypeInterface.php
+++ src/OAuth2/Grant/GrantTypeInterface.php
@@ -1,6 +1,14 @@
 <?php
 
-namespace OAuth2;
+namespace OAuth2\Grant;
+
+use OAuth2\Request;
+use OAuth2\AuthServer;
+use OAuth2\Exception;
+use OAuth2\Util\SecureKey;
+use OAuth2\Storage\SessionInterface;
+use OAuth2\Storage\ClientInterface;
+use OAuth2\Storage\ScopeInterface;
 
 interface GrantTypeInterface
 {
@@ -8,5 +16,5 @@ interface GrantTypeInterface
 
     public function getResponseType();
 
-    public function completeFlow();
+    public function completeFlow($inputParams = null, $authParams = array(), Request $request);
 }
