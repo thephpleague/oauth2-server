7bfbe81f61198cec84d335a07a43a653c6a2e458
Alex Bilbie <hello@alexbilbie.com>
2013-04-28 23:57:50 +0100

    Started PDO storage classes

---
+++ src/OAuth2/Storage/PDO/Db.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OAuth2\Storage\PDO;
+
+class Db
+{
+    public function __construct($dsn = '')
+    {
+        $db = \ezcDbFactory::create($dsn);
+        \ezcDbInstance::set($db);
+    }
+}
---
+++ src/OAuth2/Storage/PDO/Scope.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OAuth2\Storage\PDO;
+
+use OAuth2\Storage\ScopeInterface;
+
+class Scope implements ScopeInterface
+{
+    public function getScope($scope, $clientId = null, $grantType = null)
+    {
+        $db = \ezcDbInstance::get();
+
+        $stmt = $db->prepare('SELECT * FROM oauth_scopes WHERE oauth_scopes.key = :scope');
+        $stmt->bindValue(':scope', $scope);
+        $stmt->execute();
+
+        $row = $stmt->fetchObject();
+
+        if ($row === false) {
+            return false;
+        }
+
+        return array(
+            'id' =>  $row->id,
+            'scope' =>  $row->key,
+            'name'  =>  $row->name,
+            'description'  =>  $row->description
+        );
+
+    }
+}
