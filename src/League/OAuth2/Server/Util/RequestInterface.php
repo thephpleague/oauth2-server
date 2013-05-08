1df524ae6e21d6d8fa3cdd1a6569c3bb73cf15f6
Alex Bilbie <hello@alexbilbie.com>
2013-05-08 11:31:38 -0700

    Added missing docblocks

--- src/League/OAuth2/Server/Util/RequestInterface.php
+++ src/League/OAuth2/Server/Util/RequestInterface.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * OAuth 2.0 Request class interface
+ *
+ * @package     php-loep/oauth2-server
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) 2013 PHP League of Extraordinary Packages
+ * @license     http://mit-license.org/
+ * @link        http://github.com/php-loep/oauth2-server
+ */
 
 namespace League\OAuth2\Util;
 
