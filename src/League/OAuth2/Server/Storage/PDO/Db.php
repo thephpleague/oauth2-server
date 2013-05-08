fd108610657ca149cfb59106cb3643d9e3cd3419
Alex Bilbie <hello@alexbilbie.com>
2013-05-08 14:10:15 -0700

    Added missing docblock

--- src/League/OAuth2/Server/Storage/PDO/Db.php
+++ src/League/OAuth2/Server/Storage/PDO/Db.php
@@ -4,6 +4,11 @@ namespace League\OAuth2\Server\Storage\PDO;
 
 class Db
 {
+	/**
+	 * Db constructor
+	 * @param array|string $dsn Connection DSN string or array of parameters
+	 * @return void
+	 */
     public function __construct($dsn = '')
     {
         $db = \ezcDbFactory::create($dsn);
