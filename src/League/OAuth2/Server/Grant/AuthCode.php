9d1693cf782c27febc769f35e3abfe35f70168c8
Alex Bilbie <hello@alexbilbie.com>
2013-05-08 11:30:53 -0700

    Fixed incorrect @link

--- src/League/OAuth2/Server/Grant/AuthCode.php
+++ src/League/OAuth2/Server/Grant/AuthCode.php
@@ -6,7 +6,7 @@
  * @author      Alex Bilbie <hello@alexbilbie.com>
  * @copyright   Copyright (c) 2013 PHP League of Extraordinary Packages
  * @license     http://mit-license.org/
- * @link        http://github.com/php-leop/oauth2-server
+ * @link        http://github.com/php-loep/oauth2-server
  */
 
 namespace League\OAuth2\Grant;
