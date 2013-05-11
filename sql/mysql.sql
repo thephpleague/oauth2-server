3742a258f8a49e9e57fcc62795a41629e09a9df4
ziege <christoph@ziegenberg.com>
2013-05-11 14:23:20 +0300

    Updated table structure
    - Table "oauth_session_access_tokens":
      - Renamed to "oauth_access_tokens"
    - Table "oauth_session_authcodes":
      - Renamed to "oauth_authcodes"
      - Made key unique
    - Table "oauth_session_redirects":
      - Renamed to "oauth_redirects"
    - Table "oauth_session_refresh_tokens":
      - Renamed to "oauth_refresh_tokens"
      - Renamed column "session_access_token_id" to "access_token_id"
      - Removed unnecessary column "client_id" (can be used from "oauth_session")
    - Table oauth_scopes:
      - Allow NULL for name 
    - Table "oauth_session_token_scopes":
      - Renamed to "oauth_access_token_scopes"
      - Renamed column "session_access_token_id" to "access_token_id"
    - Table "oauth_session_authcode_scopes":
      - Renamed to "oauth_authcode_scopes"
      - Renamed column "oauth_session_authcode_id" to "authcode_id"
    - Several key optimizations
    - Renaming of keys

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,95 +1,100 @@
-CREATE TABLE `oauth_clients` (
-  `id` CHAR(40) NOT NULL,
-  `secret` CHAR(40) NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  `auto_approve` TINYINT(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `u_oacl_clse_clid` (`secret`,`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_access_token_scopes
+(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	access_token_id INTEGER NOT NULL,
+	scope_id SMALLINT NOT NULL
+) ENGINE=InnoDB;
+CREATE INDEX i_oaactosc_scid ON oauth_access_token_scopes (scope_id);
+CREATE UNIQUE INDEX u_oaactosc_actoid_scid ON oauth_access_token_scopes (access_token_id, scope_id);
 
-CREATE TABLE `oauth_client_endpoints` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `client_id` char(40) NOT NULL,
-  `redirect_uri` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `i_oaclen_clid` (`client_id`),
-  CONSTRAINT `f_oaclen_clid` FOREIGN KEY (`client_id`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_access_tokens
+(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	session_id INTEGER NOT NULL,
+	access_token CHAR(40) NOT NULL,
+	access_token_expires INTEGER NOT NULL
+) ENGINE=InnoDB;
+CREATE INDEX i_oaacto_seid ON oauth_access_tokens (session_id);
+CREATE UNIQUE INDEX u_oaacto_acto_seid ON oauth_access_tokens (access_token, session_id);
 
-CREATE TABLE `oauth_sessions` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `client_id` char(40) NOT NULL,
-  `owner_type` enum('user','client') NOT NULL DEFAULT 'user',
-  `owner_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `i_uase_clid_owty_owid` (`client_id`,`owner_type`,`owner_id`),
-  CONSTRAINT `f_oase_clid` FOREIGN KEY (`client_id`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_authcode_scopes
+(
+	authcode_id INTEGER NOT NULL,
+	scope_id SMALLINT NOT NULL
+) ENGINE=InnoDB;
+ALTER TABLE oauth_authcode_scopes ADD CONSTRAINT p_oauth_authcode_scopes PRIMARY KEY (authcode_id, scope_id);
+CREATE INDEX i_oaausc_auid ON oauth_authcode_scopes (authcode_id);
+CREATE INDEX i_oaausc_scid ON oauth_authcode_scopes (scope_id);
 
-CREATE TABLE `oauth_session_access_tokens` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `session_id` int(10) unsigned NOT NULL,
-  `access_token` char(40) NOT NULL,
-  `access_token_expires` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `u_oaseacto_acto_seid` (`access_token`,`session_id`),
-  KEY `f_oaseto_seid` (`session_id`),
-  CONSTRAINT `f_oaseto_seid` FOREIGN KEY (`session_id`) REFERENCES `oauth_sessions` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_authcodes
+(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	session_id INTEGER NOT NULL,
+	auth_code CHAR(40) NOT NULL,
+	auth_code_expires INTEGER NOT NULL
+) ENGINE=InnoDB;
+CREATE UNIQUE INDEX u_oaau_seid ON oauth_authcodes (session_id);
 
-CREATE TABLE `oauth_session_authcodes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `session_id` int(10) unsigned NOT NULL,
-  `auth_code` char(40) NOT NULL,
-  `auth_code_expires` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `session_id` (`session_id`),
-  CONSTRAINT `oauth_session_authcodes_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `oauth_sessions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_client_endpoints
+(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	client_id CHAR(40) NOT NULL,
+	redirect_uri VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+CREATE INDEX i_oaclen_clid ON oauth_client_endpoints (client_id);
 
-CREATE TABLE `oauth_session_redirects` (
-  `session_id` int(10) unsigned NOT NULL,
-  `redirect_uri` varchar(255) NOT NULL,
-  PRIMARY KEY (`session_id`),
-  CONSTRAINT `f_oasere_seid` FOREIGN KEY (`session_id`) REFERENCES `oauth_sessions` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_clients
+(
+	id CHAR(40) NOT NULL,
+	secret CHAR(40) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	auto_approve SMALLINT DEFAULT 0 NOT NULL
+) ENGINE=InnoDB;
+ALTER TABLE oauth_clients ADD CONSTRAINT p_oauth_clients PRIMARY KEY (id);
+CREATE UNIQUE INDEX u_oacl_se_id ON oauth_clients (secret, id);
 
-CREATE TABLE `oauth_session_refresh_tokens` (
-  `session_access_token_id` int(10) unsigned NOT NULL,
-  `refresh_token` char(40) NOT NULL,
-  `refresh_token_expires` int(10) unsigned NOT NULL,
-  `client_id` char(40) NOT NULL,
-  PRIMARY KEY (`session_access_token_id`),
-  KEY `client_id` (`client_id`),
-  CONSTRAINT `oauth_session_refresh_tokens_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `f_oasetore_setoid` FOREIGN KEY (`session_access_token_id`) REFERENCES `oauth_session_access_tokens` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_redirects
+(
+	session_id INTEGER NOT NULL,
+	redirect_uri VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+ALTER TABLE oauth_redirects ADD CONSTRAINT p_oauth_redirects PRIMARY KEY (session_id);
 
-CREATE TABLE `oauth_scopes` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `scope` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `u_oasc_sc` (`scope`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_refresh_tokens
+(
+	access_token_id INTEGER NOT NULL,
+	refresh_token CHAR(40) NOT NULL,
+	refresh_token_expires INTEGER NOT NULL
+) ENGINE=InnoDB;
+ALTER TABLE oauth_refresh_tokens ADD CONSTRAINT p_oauth_refresh_tokens PRIMARY KEY (access_token_id);
+CREATE INDEX i_oareto_actoid ON oauth_refresh_tokens (access_token_id);
 
-CREATE TABLE `oauth_session_token_scopes` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `session_access_token_id` int(10) unsigned DEFAULT NULL,
-  `scope_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `u_setosc_setoid_scid` (`session_access_token_id`,`scope_id`),
-  KEY `f_oasetosc_scid` (`scope_id`),
-  CONSTRAINT `f_oasetosc_scid` FOREIGN KEY (`scope_id`) REFERENCES `oauth_scopes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `f_oasetosc_setoid` FOREIGN KEY (`session_access_token_id`) REFERENCES `oauth_session_access_tokens` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_scopes
+(
+	id SMALLINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	scope VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NULL,
+	description VARCHAR(255) NULL
+) ENGINE=InnoDB;
+CREATE UNIQUE INDEX u_oasc_sc ON oauth_scopes (scope);
 
-CREATE TABLE `oauth_session_authcode_scopes` (
-  `oauth_session_authcode_id` int(10) unsigned NOT NULL,
-  `scope_id` smallint(5) unsigned NOT NULL,
-  KEY `oauth_session_authcode_id` (`oauth_session_authcode_id`),
-  KEY `scope_id` (`scope_id`),
-  CONSTRAINT `oauth_session_authcode_scopes_ibfk_2` FOREIGN KEY (`scope_id`) REFERENCES `oauth_scopes` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `oauth_session_authcode_scopes_ibfk_1` FOREIGN KEY (`oauth_session_authcode_id`) REFERENCES `oauth_session_authcodes` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE oauth_sessions
+(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	client_id CHAR(40) NOT NULL,
+	owner_type VARCHAR(6) DEFAULT 'user' NOT NULL,
+	owner_id VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+CREATE INDEX i_oase_clid_owty_owid ON oauth_sessions (client_id, owner_type, owner_id);
+
+
+ALTER TABLE oauth_access_token_scopes ADD CONSTRAINT f_oaactosc_actoid FOREIGN KEY (access_token_id) REFERENCES oauth_access_tokens (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_access_token_scopes ADD CONSTRAINT f_oaactosc_scid FOREIGN KEY (scope_id) REFERENCES oauth_scopes (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_access_tokens ADD CONSTRAINT f_oaacto_seid FOREIGN KEY (session_id) REFERENCES oauth_sessions (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_authcode_scopes ADD CONSTRAINT f_oaausc_auid FOREIGN KEY (authcode_id) REFERENCES oauth_authcodes (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_authcode_scopes ADD CONSTRAINT f_oaausc_scid FOREIGN KEY (scope_id) REFERENCES oauth_scopes (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_authcodes ADD CONSTRAINT f_oaau_seid FOREIGN KEY (session_id) REFERENCES oauth_sessions (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_client_endpoints ADD CONSTRAINT f_oaclen_clid FOREIGN KEY (client_id) REFERENCES oauth_clients (id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE oauth_redirects ADD CONSTRAINT f_oare_seid FOREIGN KEY (session_id) REFERENCES oauth_sessions (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_refresh_tokens ADD CONSTRAINT f_oareto_actoid FOREIGN KEY (access_token_id) REFERENCES oauth_access_tokens (id) ON UPDATE NO ACTION ON DELETE CASCADE;
+ALTER TABLE oauth_sessions ADD CONSTRAINT f_oase_clid FOREIGN KEY (client_id) REFERENCES oauth_clients (id) ON UPDATE CASCADE ON DELETE CASCADE;
