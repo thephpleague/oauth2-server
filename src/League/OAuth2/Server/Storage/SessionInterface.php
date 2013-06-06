ca4e7499864f1ff1616432b7560c410ae207888c
Garrett St. John <garrett@hellobold.com>
2013-06-06 13:09:15 -0700

    Fix to docblocks

--- src/League/OAuth2/Server/Storage/SessionInterface.php
+++ src/League/OAuth2/Server/Storage/SessionInterface.php
@@ -74,7 +74,7 @@ interface SessionInterface
      * @param  int    $sessionId   The session ID
      * @param  string $accessToken The access token
      * @param  int    $expireTime  Unix timestamp of the access token expiry time
-     * @return void
+     * @return int                 The access token ID
      */
     public function associateAccessToken($sessionId, $accessToken, $expireTime);
 
