40490db27f932441f4a65e0c356eea35580baeae
Alex Bilbie <hello@alexbilbie.com>
2013-12-24 17:01:29 +0000

    Added ServerException

---
+++ src/League/OAuth2/Server/Exception/ServerException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * OAuth 2.0 Server Exception
+ *
+ * @package     php-loep/oauth2-server
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) 2013 PHP League of Extraordinary Packages
+ * @license     http://mit-license.org/
+ * @link        http://github.com/php-loep/oauth2-server
+ */
+
+namespace League\OAuth2\Server\Exception;
+
+/**
+ * Server Exception
+ */
+class ServerException extends OAuth2Exception
+{
+
+}
