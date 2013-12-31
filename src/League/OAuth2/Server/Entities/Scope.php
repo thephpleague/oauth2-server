2d90a09f65391f45be9e930badb78e035901ee47
Alex Bilbie <hello@alexbilbie.com>
2013-12-31 15:36:02 +0000

    Scopes no longer have names

--- src/League/OAuth2/Server/Entities/Scope.php
+++ src/League/OAuth2/Server/Entities/Scope.php
@@ -6,10 +6,6 @@ class Scope
 {
     protected $id = null;
 
-    protected $scope = null;
-
-    protected $name = null;
-
     protected $description = null;
 
     public function setId($id)
@@ -21,28 +17,6 @@ class Scope
     public function getId()
     {
         return $this->id;
-    }
-
-    public function setScope($scope)
-    {
-        $this->scope = $scope;
-        return $this;
-    }
-
-    public function getScope()
-    {
-        return $this->scope;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getName()
-    {
-        return $this->name;
     }
 
     public function setDescription($description)
