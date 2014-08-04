806838b8e46cb8c1ce387dc52e0ccb1972737b04
Alex Bilbie <hello@alexbilbie.com>
2014-08-04 15:18:44 +0100

    So long codeception, you suck

--- tests/resourceServer/GetUsersInvalidTokenCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-$I = new ResourceServerTester($scenario);
-$I->wantTo('get all users with an invalid access token');
-$I->sendGET('api.php/users?access_token=foobar');
-$I->seeResponseCodeIs(401);
-$I->seeResponseIsJson();
--- tests/resourceServer/GetUsersNoTokenCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-$I = new ResourceServerTester($scenario);
-$I->wantTo('get all users without an access token');
-$I->sendGET('api.php/users');
-$I->seeResponseCodeIs(400);
-$I->seeResponseIsJson();
