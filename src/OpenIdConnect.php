77add5fde0ba9ea0ccfed3d825df61b91ebd95e0
Alessio Evangelista <e.evangelista@simgroep.nl>
2014-12-09 10:59:43 +0100

    Extending AuthorizationServer

---
+++ src/OpenIdConnect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace League\OAuth2\Server;
+
+use League\OAuth2\Server\Grant\GrantTypeInterface;
+use League\OAuth2\Server\TokenType\Bearer;
+use League\OAuth2\Server\AuthorizationServer;
+
+/**
+ * OpenId Connect class
+ *
+ * @author aevangelista
+ */
+class OpenIdConnect extends AuthorizationServer 
+{
+    //put your code here
+}
