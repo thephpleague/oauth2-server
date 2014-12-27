4ef765242d19d26c7c5f0cffaff3d1a70acc1403
Alex Bilbie <hello@alexbilbie.com>
2014-12-27 21:06:20 +0000

    Added MacTokenInterface

---
+++ src/Storage/MacTokenInterface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * OAuth 2.0 MAC Token Interface
+ *
+ * @package     league/oauth2-server
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
+ * @license     http://mit-license.org/
+ * @link        https://github.com/thephpleague/oauth2-server
+ */
+
+namespace League\OAuth2\Server\Storage;
+
+use League\OAuth2\Server\Entity\AccessTokenInterface;
+use League\OAuth2\Server\Entity\MacKeyInterface;
+
+/**
+ * MacTokenInterface
+ */
+interface MacTokenInterface extends StorageInterface
+{
+    /**
+     * Create a MAC key linked to an access token
+     * @param string                                            $macKey
+     * @param \League\OAuth2\Server\Entity\AccessTokenInterface $accessToken
+     */
+    public function create($macKey, AccessTokenInterface $accessToken);
+
+    /**
+     * Get a MAC key by access token
+     * @param string $accessToken
+     * @return string
+     */
+    public function getByAccessToken($accessToken);
+}
