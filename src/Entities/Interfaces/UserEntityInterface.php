e88d80291809f1b17873936696702af7de7bee35
Alex Bilbie <hello@alexbilbie.com>
2015-04-05 21:13:04 +0100

    Added UserEntityInterface

---
+++ src/Entities/Interfaces/UserEntityInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace League\OAuth2\Server\Entities\Interfaces;
+
+interface UserEntityInterface
+{
+    /**
+     * Return the user's identifier
+     * @return mixed
+     */
+    public function getIdentifier();
+}
