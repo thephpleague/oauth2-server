b95780022a181ed4b2b73d8cbb13f190f1e64793
Alex Bilbie <hello@alexbilbie.com>
2015-11-13 17:40:39 +0000

    Updated client credentials example

--- examples/public/client_credentials.php
+++ examples/public/client_credentials.php
@@ -1,10 +1,16 @@
 <?php
 
+use League\OAuth2\Server\Exception\OAuthException;
 use League\OAuth2\Server\Grant\ClientCredentialsGrant;
 use League\OAuth2\Server\Server;
+
 use OAuth2ServerExamples\Repositories\AccessTokenRepository;
 use OAuth2ServerExamples\Repositories\ClientRepository;
 use OAuth2ServerExamples\Repositories\ScopeRepository;
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 include(__DIR__ . '/../vendor/autoload.php');
 
@@ -18,3 +24,21 @@ $accessTokenRepository = new AccessTokenRepository();
 
 // Enable the client credentials grant on the server
 $server->enableGrantType(new ClientCredentialsGrant($clientRepository, $scopeRepository, $accessTokenRepository));
+
+// App
+$app = new App([Server::class => $server]);
+unset($app->getContainer()['errorHandler']);
+
+$app->post('/access_token', function (Request $request, Response $response) {
+    /** @var Server $server */
+    $server = $this->getContainer()->get(Server::class);
+    try {
+        return $server->respondToRequest($request);
+    } catch (OAuthException $e) {
+        return $e->generateHttpResponse();
+    } catch (\Exception $e) {
+        return $response->withStatus(500)->write($e->getMessage());
+    }
+});
+
+$app->run();
