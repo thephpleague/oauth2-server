ed17b6540ee320e72e163bf9dd8a882a1c961439
Alex Bilbie <hello@alexbilbie.com>
2016-03-22 14:44:39 +0000

    Add additional repository

--- examples/src/Repositories/UserRepository.php
+++ examples/src/Repositories/UserRepository.php
@@ -2,25 +2,34 @@
 
 namespace OAuth2ServerExamples\Repositories;
 
+use League\OAuth2\Server\Entities\Interfaces\ScopeEntityInterface;
 use League\OAuth2\Server\Repositories\UserRepositoryInterface;
+use OAuth2ServerExamples\Entities\ScopeEntity;
 use OAuth2ServerExamples\Entities\UserEntity;
 
 class UserRepository implements UserRepositoryInterface
 {
+
     /**
      * Get a user entity.
      *
-     * @param string $username
-     * @param string $password
+     * @param string                 $username
+     * @param string                 $password
+     * @param string                 $grantType The grant type used
+     * @param ScopeEntityInterface[] $scopes
      *
      * @return \League\OAuth2\Server\Entities\Interfaces\UserEntityInterface
      */
-    public function getUserEntityByUserCredentials($username, $password)
+    public function getUserEntityByUserCredentials($username, $password, $grantType, array &$scopes)
     {
         if ($username === 'alex' && $password === 'whisky') {
+            $scope = new ScopeEntity();
+            $scope->setIdentifier('email');
+            $scopes[] = $scope;
+
             return new UserEntity();
         }
 
-        return;
+        return null;
     }
 }
