c75d0e0f0ed3e24529efc9e90cc6c42e30ac9800
Alex Bilbie <hello@alexbilbie.com>
2016-04-10 11:52:18 +0100

    Removed templating code

--- src/Grant/AbstractAuthorizeGrant.php
+++ src/Grant/AbstractAuthorizeGrant.php
@@ -11,45 +11,10 @@
 
 namespace League\OAuth2\Server\Grant;
 
-use League\OAuth2\Server\TemplateRenderer\PlatesRenderer;
-use League\OAuth2\Server\TemplateRenderer\RendererInterface;
 use League\Plates\Engine;
 
 abstract class AbstractAuthorizeGrant extends AbstractGrant
 {
-    /**
-     * @var \League\OAuth2\Server\TemplateRenderer\RendererInterface
-     */
-    protected $templateRenderer;
-
-    /**
-     * Set the template renderer
-     *
-     * @param RendererInterface $templateRenderer
-     */
-    public function setTemplateRenderer(RendererInterface $templateRenderer)
-    {
-        $this->templateRenderer = $templateRenderer;
-    }
-
-    /**
-     * Retrieve template renderer.
-     *
-     * @return \League\OAuth2\Server\TemplateRenderer\RendererInterface
-     */
-    protected function getTemplateRenderer()
-    {
-        if (!$this->templateRenderer instanceof RendererInterface) {
-            $this->templateRenderer = new PlatesRenderer(
-                new Engine(__DIR__ . '/../TemplateRenderer/DefaultTemplates'),
-                'login_user',
-                'authorize_client'
-            );
-        }
-
-        return $this->templateRenderer;
-    }
-
     /**
      * @param string $uri
      * @param array  $params
