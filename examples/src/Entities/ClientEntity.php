651709b70fa070428d08c3d00c23052b641d4109
Alex Bilbie <hello@alexbilbie.com>
2016-04-10 11:47:15 +0100

    Added helper methods

--- examples/src/Entities/ClientEntity.php
+++ examples/src/Entities/ClientEntity.php
@@ -9,4 +9,14 @@ use League\OAuth2\Server\Entities\Traits\EntityTrait;
 class ClientEntity implements ClientEntityInterface
 {
     use EntityTrait, ClientTrait;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setRedirectUri($uri)
+    {
+        $this->redirectUri = $uri;
+    }
 }
