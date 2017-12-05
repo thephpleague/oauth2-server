61a3466a2c724b1208926cf496c0fe24100716de
Martin Ossowski <martin.ossowski@trivago.com>
2017-12-05 14:50:33 +0100

    cleanup AbstractJwtAwareRefreshToken

--- src/Entities/AbstractJwtAwareRefreshToken.php
+++ src/Entities/AbstractJwtAwareRefreshToken.php
@@ -1,6 +1,11 @@
 <?php
-
-declare(strict_types = 1);
+/**
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
+ * @license     http://mit-license.org/
+ *
+ * @link        https://github.com/thephpleague/oauth2-server
+ */
 
 namespace League\OAuth2\Server\Entities;
 
