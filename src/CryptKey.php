06a23a1dd0a46a49bb7078f4864a5f8edb677291
Andrew Millington <andrew@noexceptions.io>
2018-02-11 22:12:55 +0000

    Update CryptKey.php
    Change the error message to reflect that the server will also accept 440 and 400 as a valid file permission

--- src/CryptKey.php
+++ src/CryptKey.php
@@ -50,7 +50,7 @@ class CryptKey
             $keyPathPerms = decoct(fileperms($keyPath) & 0777);
             if (in_array($keyPathPerms, ['400', '440', '600', '660'], true) === false) {
                 trigger_error(sprintf(
-                    'Key file "%s" permissions are not correct, should be 600 or 660 instead of %s',
+                    'Key file "%s" permissions are not correct, recommend changing to 600 or 660 instead of %s',
                     $keyPath,
                     $keyPathPerms
                 ), E_USER_NOTICE);
