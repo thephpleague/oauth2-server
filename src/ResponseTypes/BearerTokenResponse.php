1a6ebdf81c9561bb2fff3c40abff8f519c68c611
sephster <andrew@noexceptions.io>
2019-07-02 19:24:19 +0100

    Fix order of imports

--- src/ResponseTypes/BearerTokenResponse.php
+++ src/ResponseTypes/BearerTokenResponse.php
@@ -13,8 +13,8 @@ namespace League\OAuth2\Server\ResponseTypes;
 
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
-use Psr\Http\Message\ResponseInterface;
 use LogicException;
+use Psr\Http\Message\ResponseInterface;
 
 class BearerTokenResponse extends AbstractResponseType
 {
