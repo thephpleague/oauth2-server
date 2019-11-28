25454751dd403adc507b3612b0ee38ed69437d0a
Luca Degasperi <luca@lucadegasperi.com>
2019-11-28 10:28:16 +0100

    Fix Authorship

--- src/Grant/DeviceCodeGrant.php
+++ src/Grant/DeviceCodeGrant.php
@@ -2,8 +2,8 @@
 /**
  * OAuth 2.0 Device Code grant.
  *
- * @author      Luca Degasperi <luca@lucadegasperi.com>
- * @copyright   Copyright (c) Luca Degasperi
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
  * @license     http://mit-license.org/
  *
  * @link        https://github.com/thephpleague/oauth2-server
--- src/Repositories/DeviceCodeRepositoryInterface.php
+++ src/Repositories/DeviceCodeRepositoryInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author      Luca Degasperi <luca@lucadegasperi.com>
- * @copyright   Copyright (c) Luca Degasperi
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
  * @license     http://mit-license.org/
  *
  * @link        https://github.com/thephpleague/oauth2-server
