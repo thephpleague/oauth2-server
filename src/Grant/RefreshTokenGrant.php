c6c278882c15f446a4b547ab8aac23889641cffb
Amauri de Melo Junior <amauri.melo@cloudbeds.com>
2020-04-08 13:34:24 -0300

    Cleanup and check fix

--- src/Grant/RefreshTokenGrant.php
+++ src/Grant/RefreshTokenGrant.php
@@ -5,6 +5,7 @@
  * @author      Alex Bilbie <hello@alexbilbie.com>
  * @copyright   Copyright (c) Alex Bilbie
  * @license     http://mit-license.org/
+ *
  * @link        https://github.com/thephpleague/oauth2-server
  */
 
@@ -112,7 +113,6 @@ class RefreshTokenGrant extends AbstractGrant
         return $this->requireClientSecret;
     }
 
-
     /**
      * {@inheritdoc}
      */
@@ -141,6 +141,7 @@ class RefreshTokenGrant extends AbstractGrant
 
         if (($client instanceof ClientEntity) === false) {
             $this->server->getEventEmitter()->emit(new Event\ClientAuthenticationFailedEvent($this->server->getRequest()));
+
             throw new Exception\InvalidClientException();
         }
 
