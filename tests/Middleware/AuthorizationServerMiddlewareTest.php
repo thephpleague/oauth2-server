4475fd699a93edcecf19c9238aec10de0aa960d1
sephster <andrew@noexceptions.io>
2020-09-23 21:38:38 +0100

    Apply StyleCI fix

--- tests/Middleware/AuthorizationServerMiddlewareTest.php
+++ tests/Middleware/AuthorizationServerMiddlewareTest.php
@@ -109,8 +109,10 @@ class AuthorizationServerMiddlewareTest extends TestCase
         $response = $exception->generateHttpResponse(new Response());
 
         $this->assertEquals(302, $response->getStatusCode());
-        $this->assertEquals('http://foo/bar?error=invalid_scope&error_description=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed&hint=Check+the+%60test%60+scope&message=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed',
-            $response->getHeader('location')[0]);
+        $this->assertEquals(
+            'http://foo/bar?error=invalid_scope&error_description=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed&hint=Check+the+%60test%60+scope&message=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed',
+            $response->getHeader('location')[0]
+        );
     }
 
     public function testOAuthErrorResponseRedirectUriFragment()
@@ -119,7 +121,9 @@ class AuthorizationServerMiddlewareTest extends TestCase
         $response = $exception->generateHttpResponse(new Response(), true);
 
         $this->assertEquals(302, $response->getStatusCode());
-        $this->assertEquals('http://foo/bar#error=invalid_scope&error_description=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed&hint=Check+the+%60test%60+scope&message=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed',
-            $response->getHeader('location')[0]);
+        $this->assertEquals(
+            'http://foo/bar#error=invalid_scope&error_description=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed&hint=Check+the+%60test%60+scope&message=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed',
+            $response->getHeader('location')[0]
+        );
     }
 }
