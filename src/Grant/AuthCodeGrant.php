7157bff6fefc3f8f822b48f2aeb16c3c463eaf61
Lukáš Unger <looky.msc@gmail.com>
2021-04-06 22:18:57 +0200

    Added token events

--- src/Grant/AuthCodeGrant.php
+++ src/Grant/AuthCodeGrant.php
@@ -20,7 +20,9 @@ use League\OAuth2\Server\Entities\UserEntityInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\Repositories\AuthCodeRepositoryInterface;
 use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
+use League\OAuth2\Server\RequestAccessTokenEvent;
 use League\OAuth2\Server\RequestEvent;
+use League\OAuth2\Server\RequestRefreshTokenEvent;
 use League\OAuth2\Server\RequestTypes\AuthorizationRequest;
 use League\OAuth2\Server\ResponseTypes\RedirectResponse;
 use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
@@ -162,14 +164,14 @@ class AuthCodeGrant extends AbstractAuthorizeGrant
 
         // Issue and persist new access token
         $accessToken = $this->issueAccessToken($accessTokenTTL, $client, $authCodePayload->user_id, $scopes);
-        $this->getEmitter()->emit(new RequestEvent(RequestEvent::ACCESS_TOKEN_ISSUED, $request));
+        $this->getEmitter()->emit(new RequestAccessTokenEvent(RequestEvent::ACCESS_TOKEN_ISSUED, $request, $accessToken));
         $responseType->setAccessToken($accessToken);
 
         // Issue and persist new refresh token if given
         $refreshToken = $this->issueRefreshToken($accessToken);
 
         if ($refreshToken !== null) {
-            $this->getEmitter()->emit(new RequestEvent(RequestEvent::REFRESH_TOKEN_ISSUED, $request));
+            $this->getEmitter()->emit(new RequestRefreshTokenEvent(RequestEvent::REFRESH_TOKEN_ISSUED, $request, $refreshToken));
             $responseType->setRefreshToken($refreshToken);
         }
 
---
+++ src/RequestRefreshTokenEvent.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
+ * @license     http://mit-license.org/
+ *
+ * @link        https://github.com/thephpleague/oauth2-server
+ */
+
+namespace League\OAuth2\Server;
+
+use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RequestRefreshTokenEvent extends RequestEvent
+{
+    /**
+     * @var RefreshTokenEntityInterface
+     */
+    private $refreshToken;
+
+    /**
+     * @param string                 $name
+     * @param ServerRequestInterface $request
+     */
+    public function __construct($name, ServerRequestInterface $request, RefreshTokenEntityInterface $refreshToken)
+    {
+        parent::__construct($name, $request);
+        $this->refreshToken = $refreshToken;
+    }
+
+    /**
+     * @return RefreshTokenEntityInterface
+     * @codeCoverageIgnore
+     */
+    public function getRefreshToken()
+    {
+        return $this->refreshToken;
+    }
+}
