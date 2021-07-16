34a170c5d9c1907074961d2c1374b1fc34dff442
Jesse Donat <donatj@gmail.com>
2021-07-16 16:12:31 -0500

    Simplifies `Bearer` regex
    `(?:\s+)?` is just `\s*` but harder

--- src/AuthorizationValidators/BearerTokenValidator.php
+++ src/AuthorizationValidators/BearerTokenValidator.php
@@ -94,7 +94,7 @@ class BearerTokenValidator implements AuthorizationValidatorInterface
         }
 
         $header = $request->getHeader('authorization');
-        $jwt = \trim((string) \preg_replace('/^(?:\s+)?Bearer\s/', '', $header[0]));
+        $jwt = \trim((string) \preg_replace('/^\s*Bearer\s/', '', $header[0]));
 
         try {
             // Attempt to parse the JWT
