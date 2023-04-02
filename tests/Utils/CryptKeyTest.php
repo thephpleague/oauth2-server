a77188b7fedf2b342ffa419ba2171cb9f154b464
Andrew Millington <andrew@noexceptions.io>
2023-04-02 23:42:30 +0100

    revoke test

--- tests/Utils/CryptKeyTest.php
+++ tests/Utils/CryptKeyTest.php
@@ -64,7 +64,6 @@ class CryptKeyTest extends TestCase
                 'private_key_bits' => 2048,
                 'private_key_type' => OPENSSL_KEYTYPE_DSA,
             ]);
-            die(var_dump($res));
             // Get private key
             \openssl_pkey_export($res, $keyContent, 'mystrongpassword');
             $path = self::generateKeyPath($keyContent);
