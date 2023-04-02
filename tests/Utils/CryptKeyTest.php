ff7d4ab60c8ce793ff21e025f63e8be2b20744eb
Andrew Millington <andrew@noexceptions.io>
2023-04-02 23:46:23 +0100

    echo error string

--- tests/Utils/CryptKeyTest.php
+++ tests/Utils/CryptKeyTest.php
@@ -64,6 +64,7 @@ class CryptKeyTest extends TestCase
                 'private_key_bits' => 2048,
                 'private_key_type' => OPENSSL_KEYTYPE_DSA,
             ]);
+            die(openssl_error_string());
             // Get private key
             \openssl_pkey_export($res, $keyContent, 'mystrongpassword');
             $path = self::generateKeyPath($keyContent);
