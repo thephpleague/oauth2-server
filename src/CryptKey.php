f697da198a1adf892c83fd8123198bf2199ee265
Pradeep Kumar Reddy <pradeep.kumar@pantheon.io>
2023-11-07 22:40:49 +0530

    Fixing permission error

--- src/CryptKey.php
+++ src/CryptKey.php
@@ -70,7 +70,7 @@ class CryptKey
         if ($keyPermissionsCheck === true) {
             // Verify the permissions of the key
             $keyPathPerms = \decoct(\fileperms($this->keyPath) & 0777);
-            if (\in_array($keyPathPerms, ['400', '440', '600', '640', '660'], true) === false) {
+            if (\in_array($keyPathPerms, ['600', '660'], true) === false) {
                 \trigger_error(
                     \sprintf(
                         'Key file "%s" permissions are not correct, recommend changing to 600 or 660 instead of %s',
