d00fbfcebf7905bbce61f6d1e6d44468fcad0926
Andrew Millington <andrew@noexceptions.io>
2024-03-21 16:37:26 +0000

    Update author

--- src/Entities/Traits/DeviceCodeTrait.php
+++ src/Entities/Traits/DeviceCodeTrait.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @author      Andrew Millington <andrew@noexceptions.io>
  * @copyright   Copyright (c) Alex Bilbie
  * @license     http://mit-license.org/
  *
