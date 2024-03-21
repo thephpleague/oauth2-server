dde13a424f2aa28a26f1a429038e97fa032f6107
Andrew Millington <andrew@noexceptions.io>
2024-03-21 16:35:20 +0000

    Revert docblock deletions

--- src/Entities/TokenInterface.php
+++ src/Entities/TokenInterface.php
@@ -16,12 +16,24 @@ use DateTimeImmutable;
 
 interface TokenInterface
 {
+    /**
+     * Get the token's identifier.
+     */
     public function getIdentifier(): string;
 
+    /**
+     * Set the token's identifier.
+     */
     public function setIdentifier(mixed $identifier): void;
 
+    /**
+     * Get the token's expiry date time.
+     */
     public function getExpiryDateTime(): DateTimeImmutable;
 
+    /**
+     * Set the date time when the token expires.
+     */
     public function setExpiryDateTime(DateTimeImmutable $dateTime): void;
 
     /**
@@ -31,15 +43,29 @@ interface TokenInterface
      */
     public function setUserIdentifier(string $identifier): void;
 
+    /**
+     * Get the token user's identifier.
+     */
     public function getUserIdentifier(): string|int|null;
 
+    /**
+     * Get the client that the token was issued to.
+     */
     public function getClient(): ClientEntityInterface;
 
+    /**
+     * Set the client that the token was issued to.
+     */
     public function setClient(ClientEntityInterface $client): void;
 
+    /**
+     * Associate a scope with the token.
+     */
     public function addScope(ScopeEntityInterface $scope): void;
 
     /**
+     * Return an array of scopes associated with the token.
+     *
      * @return ScopeEntityInterface[]
      */
     public function getScopes(): array;
