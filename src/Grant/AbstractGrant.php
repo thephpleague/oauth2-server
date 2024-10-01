643826f7d1657ee0fee338c4287056b1cff2dd60
Hafez Divandari <hafezdivandari@gmail.com>
2024-10-01 17:08:55 +0330

    add ClientEntityInterface::hasGrantType()

--- src/Grant/AbstractGrant.php
+++ src/Grant/AbstractGrant.php
@@ -496,7 +496,7 @@ abstract class AbstractGrant implements GrantTypeInterface
 
         $refreshToken = $this->refreshTokenRepository->getNewRefreshToken();
 
-        if ($refreshToken === null) {
+        if ($refreshToken === null || !$accessToken->getClient()->hasGrantType('refresh_token')) {
             return null;
         }
 
