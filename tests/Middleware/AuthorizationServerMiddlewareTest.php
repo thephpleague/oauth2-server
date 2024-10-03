02691dc2f5350305b18f24031bf1a714194135e9
Hafez Divandari <hafezdivandari@gmail.com>
2024-10-03 15:49:22 +0330

    wip

--- tests/Middleware/AuthorizationServerMiddlewareTest.php
+++ tests/Middleware/AuthorizationServerMiddlewareTest.php
@@ -133,16 +133,4 @@ class AuthorizationServerMiddlewareTest extends TestCase
             $response->getHeader('location')[0]
         );
     }
-
-    public function testOAuthErrorResponseRedirectUriWithDelimiter(): void
-    {
-        $exception = OAuthServerException::invalidScope('test', 'http://foo/bar');
-        $response = $exception->generateHttpResponse(new Response(), true);
-
-        self::assertEquals(302, $response->getStatusCode());
-        self::assertEquals(
-            'http://foo/bar#error=invalid_scope&error_description=The+requested+scope+is+invalid%2C+unknown%2C+or+malformed&hint=Check+the+%60test%60+scope',
-            $response->getHeader('location')[0]
-        );
-    }
 }
