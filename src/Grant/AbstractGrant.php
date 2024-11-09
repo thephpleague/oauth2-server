dfef82fdd2f95c62846ff1f73e317f986351f0f1
Hafez Divandari <hafezdivandari@gmail.com>
2024-11-09 23:34:40 +0330

    redirect uri is required on auth code

--- src/Grant/AbstractGrant.php
+++ src/Grant/AbstractGrant.php
@@ -155,13 +155,6 @@ abstract class AbstractGrant implements GrantTypeInterface
             }
         }
 
-        // If a redirect URI is provided ensure it matches what is pre-registered
-        $redirectUri = $this->getRequestParameter('redirect_uri', $request);
-
-        if ($redirectUri !== null) {
-            $this->validateRedirectUri($redirectUri, $client, $request);
-        }
-
         return $client;
     }
 
@@ -223,13 +216,13 @@ abstract class AbstractGrant implements GrantTypeInterface
      * @throws OAuthServerException
      */
     protected function validateRedirectUri(
-        string $redirectUri,
+        ?string $redirectUri,
         ClientEntityInterface $client,
         ServerRequestInterface $request
     ): void {
         $validator = new RedirectUriValidator($client->getRedirectUri());
 
-        if (!$validator->validateRedirectUri($redirectUri)) {
+        if (is_null($redirectUri) || !$validator->validateRedirectUri($redirectUri)) {
             $this->getEmitter()->emit(new RequestEvent(RequestEvent::CLIENT_AUTHENTICATION_FAILED, $request));
             throw OAuthServerException::invalidClient($request);
         }
