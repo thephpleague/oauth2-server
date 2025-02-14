163834bd6d4396c66487cc7168cda5233eaa61a4
Phillip Look <phillip.look@brainbits.net>
2025-02-14 17:33:07 +0100

    Remove redundant code

--- src/Grant/RefreshTokenGrant.php
+++ src/Grant/RefreshTokenGrant.php
@@ -83,10 +83,6 @@ class RefreshTokenGrant extends AbstractGrant
         }
 
         // Issue and persist new access token
-        $userId = $oldRefreshToken['user_id'];
-        if (is_int($userId)) {
-            $userId = (string) $userId;
-        }
         $accessToken = $this->issueAccessToken($accessTokenTTL, $client, $userId, $scopes);
         $this->getEmitter()->emit(new RequestAccessTokenEvent(RequestEvent::ACCESS_TOKEN_ISSUED, $request, $accessToken));
         $responseType->setAccessToken($accessToken);
